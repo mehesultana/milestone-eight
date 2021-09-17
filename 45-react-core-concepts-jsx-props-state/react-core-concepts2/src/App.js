0b8c2f2934a8485efb52d68b593b927d592f26a9
mehesultana <sultanamehe01@gmail.com>
2021-09-18 02:29:11 +0600

    Display Array Of Objects

--- 45-react-core-concepts-jsx-props-state/react-core-concepts2/src/App.js
+++ 45-react-core-concepts-jsx-props-state/react-core-concepts2/src/App.js
@@ -1,8 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return <div className="App"></div>;
+	const products = [
+		{ name: 'mobile', price: 15000 },
+		{ name: 'camera', price: 55000 },
+		{ name: 'laptop', price: 150000 },
+		{ name: 'watch', price: 5000 },
+		{ name: 'shoe', price: 500 },
+	];
+	return (
+		<div className="App">
+			{products.map((product) => (
+				<Product name={product.name} price={product.price}></Product>
+			))}
+
+			{/* <Product name="mobile" price="15000"></Product>
+			<Product name="laptop" price="15000"></Product>
+			<Product name="camera" price="15000"></Product>
+			<Product name="mobile" price="15000"></Product>
+			<Product name="mobile" price="15000"></Product> */}
+		</div>
+	);
 }
 
+function Product(props) {
+	const productStyle = {
+		border: '3px solid blue',
+		borderRadius: '10px',
+	};
+	return (
+		<div className="product" style={productStyle}>
+			<h2>Name: {props.name}</h2>
+			<h4>Price: {props.price}</h4>
+		</div>
+	);
+}
 export default App;
