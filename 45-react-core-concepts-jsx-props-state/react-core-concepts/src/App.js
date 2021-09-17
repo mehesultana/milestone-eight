2e5db4e2b6b728132508e96e9c0e4869657b6e61
mehesultana <sultanamehe01@gmail.com>
2021-09-18 01:10:16 +0600

    pass Dynamic Data To Components

--- 45-react-core-concepts-jsx-props-state/react-core-concepts/src/App.js
+++ 45-react-core-concepts-jsx-props-state/react-core-concepts/src/App.js
@@ -3,24 +3,41 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<Person></Person>
-			<Person></Person>
-			<Person></Person>
+			<Friend phone="0127407315" address="Noakhali"></Friend>
+			<Friend phone="0832479490" address="Rongpur"></Friend>
+			<Friend phone="0143985488" address="Jessor"></Friend>
+			<Friend phone="0346457450" address="Dhaka"></Friend>
 		</div>
 	);
 }
 
-function Person() {
+/* 
+      <p id="title">Title</p>
+			<Person name="Rubel" nayika="Moushumi"></Person>
+			<Person name="BappaRaz" nayika="Cheka"></Person>
+			<Person name="Kuber" nayika="Kopila"></Person>
+*/
+function Person(props) {
+	console.log(props);
 	const person = {
 		backgroundColor: 'skyblue',
 		border: '3px solid lightpink',
-		borderRadius: '20px',
+		borderRadius: '10px',
 		margin: '10px',
 	};
 	return (
 		<div style={person}>
-			<h1>Mehe</h1>
-			<h4>profession: Student</h4>
+			<h1>Name: {props.name}</h1>
+			<h4>Hero of: {props.nayika}</h4>
+		</div>
+	);
+}
+function Friend(props) {
+	console.log(props);
+	return (
+		<div className="person">
+			<h3>phone: {props.phone} </h3>
+			<h5>Address: {props.address}</h5>
 		</div>
 	);
 }
