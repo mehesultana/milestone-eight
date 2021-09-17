ae3f82c9c786e81b0a0827d61e9d7f71d44ba55d
mehesultana <sultanamehe01@gmail.com>
2021-09-18 00:43:48 +0600

    Return Html From A Component

--- 45-react-core-concepts-jsx-props-state/react-core-concepts/src/App.js
+++ 45-react-core-concepts-jsx-props-state/react-core-concepts/src/App.js
@@ -1,46 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	const number = 555;
-	const singer = {
-		name: 'Dr. Mahfuz',
-		job: 'singer',
-	};
-	const singer2 = {
-		name: 'Eva Rahman',
-	};
-	const singerStyle = {
-		backgroundColor: 'red',
-		color: 'white',
-	};
-
 	return (
 		<div className="App">
-			<header className="App-header">
-				<h3>reac</h3>
-				<p>happy React</p>
-				<div className="container">
-					<h3>this is inside my container</h3>
-					<p>my number is: {number}</p>
-					<p
-						style={{
-							backgroundColor: 'gray',
-							color: 'megenta',
-						}}
-					>
-						singer: {singer.name} {singer.job}
-					</p>
-					<p style={singerStyle}>Real singer: {singer2.name}</p>
-				</div>
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<Person></Person>
+			<Person></Person>
+			<Person></Person>
+		</div>
+	);
+}
+
+function Person() {
+	const person = {
+		backgroundColor: 'skyblue',
+		border: '3px solid lightpink',
+		borderRadius: '20px',
+		margin: '10px',
+	};
+	return (
+		<div style={person}>
+			<h1>Mehe</h1>
+			<h4>profession: Student</h4>
 		</div>
 	);
 }
