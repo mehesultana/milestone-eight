d46d10ed6fe745f68823652ee3039df1cba90d8b
mehesultana <sultanamehe01@gmail.com>
2021-09-19 01:27:59 +0600

    Event listener, useState inside components

--- 45.5-react-core-concept-recap-and-practice-day/react-core-concepts-recap/src/App.js
+++ 45.5-react-core-concept-recap-and-practice-day/react-core-concepts-recap/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 	return (
@@ -11,17 +12,26 @@ function App() {
 }
 
 function MyComponent(props) {
-	console.log(props);
+	const [points, setPoints] = useState(1);
+	// console.log(props);
 	const myStyle = {
 		backgroundColor: 'lightblue',
 		border: '3px solid blue',
-		margin: '20px',
-		padding: '10px',
+		margin: '10px',
+		padding: '5px',
+	};
+	const handleAddPoinst = () => {
+		// console.log('click');
+		const newPoints = points * 2;
+		setPoints(newPoints);
 	};
 	return (
 		<div style={myStyle}>
 			<h1>My own Component {props.brand}</h1>
-			<h2>Asking money: {props.price}</h2>
+			<h2>
+				Asking money: price: {props.price} , I have points: {points}
+			</h2>
+			<button onClick={handleAddPoinst}>Add points</button>
 			<p style={{ color: 'hotpink' }}>I can wright my own Component</p>
 		</div>
 	);
