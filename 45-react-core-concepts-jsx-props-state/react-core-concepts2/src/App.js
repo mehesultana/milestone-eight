dab6abc6583a76681166865115992c51b3949419
mehesultana <sultanamehe01@gmail.com>
2021-09-18 20:00:26 +0600

    Component State Hook And Set State Method

--- 45-react-core-concepts-jsx-props-state/react-core-concepts2/src/App.js
+++ 45-react-core-concepts-jsx-props-state/react-core-concepts2/src/App.js
@@ -1,38 +1,56 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-	const products = [
-		{ name: 'mobile', price: 15000 },
-		{ name: 'camera', price: 55000 },
-		{ name: 'laptop', price: 150000 },
-		{ name: 'watch', price: 5000 },
-		{ name: 'shoe', price: 500 },
-	];
 	return (
 		<div className="App">
-			{products.map((product) => (
-				<Product name={product.name} price={product.price}></Product>
-			))}
-
-			{/* <Product name="mobile" price="15000"></Product>
-			<Product name="laptop" price="15000"></Product>
-			<Product name="camera" price="15000"></Product>
-			<Product name="mobile" price="15000"></Product>
-			<Product name="mobile" price="15000"></Product> */}
+			<Counter></Counter>
 		</div>
 	);
 }
 
-function Product(props) {
-	const productStyle = {
-		border: '3px solid blue',
-		borderRadius: '10px',
-	};
+function Counter() {
+	const [count, setCount] = useState(0);
+	const handleIncrease = () => setCount(count + 1);
+	const handleDecrease = () => setCount(count - 1);
 	return (
-		<div className="product" style={productStyle}>
-			<h2>Name: {props.name}</h2>
-			<h4>Price: {props.price}</h4>
+		<div>
+			<h1>Count : {count}</h1>
+			<button onClick={handleIncrease}>Increase</button>
+			<button onClick={handleDecrease}>Decrease</button>
 		</div>
 	);
 }
+
+// const products = [
+// 		{ name: 'mobile', price: 15000 },
+// 		{ name: 'camera', price: 55000 },
+// 		{ name: 'laptop', price: 150000 },
+// 		{ name: 'watch', price: 5000 },
+// 		{ name: 'shoe', price: 500 },
+// 	];
+
+// {products.map((product) => (
+// 				<Product name={product.name} price={product.price}></Product>
+// 			))}
+
+// 			<Product name="mobile" price="15000"></Product>
+// 			<Product name="laptop" price="15000"></Product>
+// 			<Product name="camera" price="15000"></Product>
+// 			<Product name="mobile" price="15000"></Product>
+// 			<Product name="mobile" price="15000"></Product>
+
+// function Product(props) {
+// 	const productStyle = {
+// 		border: '3px solid blue',
+// 		borderRadius: '10px',
+// 	};
+// 	return (
+// 		<div className="product" style={productStyle}>
+// 			<h2>Name: {props.name}</h2>
+// 			<h4>Price: {props.price}</h4>
+// 		</div>
+// 	);
+// }
+
 export default App;
