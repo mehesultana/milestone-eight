7aa8859fd363b1a5881a6f7cd7f58bdafc64180e
mehesultana <sultanamehe01@gmail.com>
2021-09-19 01:52:14 +0600

    Load data using the useEffect and display data

--- 45.5-react-core-concept-recap-and-practice-day/react-core-concepts-recap/src/App.js
+++ 45.5-react-core-concept-recap-and-practice-day/react-core-concepts-recap/src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 	return (
 		<div className="App">
+			<LoadUsers></LoadUsers>
 			<MyComponent brand="Apple" price="5000"></MyComponent>
 			<MyComponent brand="Macrosoft" price="1000"></MyComponent>
 			<MyComponent brand="Google" price="0"></MyComponent>
+		</div>
+	);
+}
+
+function LoadUsers() {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
+	}, []);
+	return (
+		<div>
+			<h1>Users Loaded: {users.length}</h1>
+			{users.map((user) => (
+				<User name={user.name} phone={user.phone}></User>
+			))}
+		</div>
+	);
+}
+
+function User(props) {
+	return (
+		<div className="user">
+			<h2>Name: {props.name}</h2>
+			<p>Call me {props.phone}</p>
 		</div>
 	);
 }
