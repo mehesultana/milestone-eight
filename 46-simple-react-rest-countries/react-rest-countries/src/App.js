a787b9366305124285b230b6662757b0ebc4bec4
mehesultana <sultanamehe01@gmail.com>
2021-09-19 23:20:12 +0600

    create react app

---
+++ 46-simple-react-rest-countries/react-rest-countries/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<Countries></Countries>
+		</div>
+	);
+}
+function Countries() {
+	return (
+		<div>
+			<h1>Traveling around the world</h1>
+		</div>
+	);
+}
+export default App;
