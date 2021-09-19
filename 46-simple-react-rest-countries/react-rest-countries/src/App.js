57e0edfecf04701bb62eef8bbd6424b4d63fe7e1
mehesultana <sultanamehe01@gmail.com>
2021-09-19 23:47:11 +0600

    Display countries in a simple way, Folder Structure

--- 46-simple-react-rest-countries/react-rest-countries/src/App.js
+++ 46-simple-react-rest-countries/react-rest-countries/src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 
 function App() {
-	return (
-		<div className="App">
-			<Countries></Countries>
-		</div>
-	);
+	return <div className="App"></div>;
 }
-function Countries() {
+/* function Countries() {
 	const [countries, setCountries] = useState([]);
 	useEffect(() => {
 		fetch('https://restcountries.eu/rest/v2/all')
@@ -19,7 +14,19 @@ function Countries() {
 		<div>
 			<h1>Traveling around the world</h1>
 			<h4>Countries Available: {countries.length}</h4>
+			{countries.map((country) => (
+				<Country name={country.name} capital={country.capital}></Country>
+			))}
 		</div>
 	);
 }
+
+function Country(props) {
+	return (
+		<div>
+			<h2>Name: {props.name}</h2>
+			<p>Capital: {props.capital}</p>
+		</div>
+	);
+} */
 export default App;
