b6086f7f695c9e9cddaa8f04891ad92793f249f0
mehesultana <sultanamehe01@gmail.com>
2021-09-20 21:24:58 +0600

    done rest countries

--- 46-simple-react-rest-countries/react-rest-countries2/src/App.js
+++ 46-simple-react-rest-countries/react-rest-countries2/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Countries from './components/Countries/Countries';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>hello</h1>
+			<Countries></Countries>
 		</div>
 	);
 }
---
+++ 46-simple-react-rest-countries/react-rest-countries2/src/components/Country/Country.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Country.css';
+
+const Country = (props) => {
+	const { name, flag, capital, population, region } = props.country;
+	return (
+		<div className="country ">
+			<h4>This is : {name}</h4>
+			<img src={flag} alt="" srcset="" />
+			<p>Region : {region}</p>
+			<p>
+				Capital is : {capital} population : {population}
+			</p>
+		</div>
+	);
+};
+
+export default Country;
