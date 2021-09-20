7a8840c898c92091dfccb3afa017f5de3966d8fc
mehesultana <sultanamehe01@gmail.com>
2021-09-21 02:12:11 +0600

    asynchronous, console state

--- 47-how-react-works/understanding-react/src/App.js
+++ 47-how-react-works/understanding-react/src/App.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Device from './components/Device/Device';
 
 function App() {
+	const [steps, setSteps] = useState(0);
+	const handleIncreaseSteps = () => {
+		const newSteps = steps + 1;
+		setSteps(newSteps);
+	};
+	useEffect(() => {
+		console.log(steps);
+	}, [steps]);
 	return (
 		<div className="App">
+			<h3>My Steps: {steps}</h3>
+			<button onClick={handleIncreaseSteps}>Walk</button>
 			<Device name="phone" price="13000"></Device>
 		</div>
 	);
