05eb55689ffc96c868c55602bcf9f1d332a53f93
mehesultana <sultanamehe01@gmail.com>
2021-09-22 00:43:54 +0600

    create components , import, export

---
+++ 47.5-practice-and-react-side-things/react-side-things/src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { add, multiply } from '../../utilities/storage';
+const Cosmetics = () => {
+	const first = 55;
+	const second = 89;
+	const sum = add(first, second);
+	const multi = multiply(first, second);
+	return <div></div>;
+};
+
+export default Cosmetics;
