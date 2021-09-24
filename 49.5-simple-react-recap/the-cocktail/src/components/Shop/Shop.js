7a21df63287cb86afd797f7272101d4b20918ec9
mehesultana <sultanamehe01@gmail.com>
2021-09-25 00:23:32 +0600

    Shop Component Layout

---
+++ 49.5-simple-react-recap/the-cocktail/src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+	return (
+		<div className="shop-container">
+			<div className="cocktail-container">
+				<h2>Cocktail</h2>
+			</div>
+			<div className="cart-container">
+				<h3>My Drinks</h3>
+				<h5>My Items</h5>
+			</div>
+		</div>
+	);
+};
+
+export default Shop;
